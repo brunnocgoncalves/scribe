4db17122293d35a4846bba7f1b427bc02ebd3d18
Brunno Goncalves <brunnocgoncalves@gmail.com>
2016-11-17 12:31:09 -0200

    Addded transcription function and finished app

--- Scribe/ViewController.swift
+++ Scribe/ViewController.swift
@@ -7,19 +7,86 @@
 //
 
 import UIKit
+import Speech
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    
+    @IBOutlet weak var activitySpinner: UIActivityIndicatorView!
+    @IBOutlet weak var trascriptionText: UITextView!
+    
+    var audioPlayer: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        activitySpinner.isHidden = true
+        
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        player.stop()
+        activitySpinner.stopAnimating()
+        activitySpinner.isHidden = true
+
+    }
+    
+    func requestSpeechAuth(){
+        
+        SFSpeechRecognizer.requestAuthorization { authStatus in
+            
+            if authStatus == SFSpeechRecognizerAuthorizationStatus.authorized{
+                
+                if let path = Bundle.main.url(forResource: "test", withExtension: "m4a"){
+                    
+                    do{
+                        
+                        let sound = try AVAudioPlayer(contentsOf: path)
+                        self.audioPlayer = sound
+                        sound.play()
+                        
+                    } catch{
+                        
+                        print("Error")
+                        
+                    }
+                    
+                    let recognizer = SFSpeechRecognizer()
+                    let request = SFSpeechURLRecognitionRequest(url: path)
+                    recognizer?.recognitionTask(with: request){ (result, error) in
+                    
+                        if let error = error{
+                            
+                            print(error)
+                            
+                        } else{
+                            
+                            if let resultString = result?.bestTranscription.formattedString{
+                                
+                                self.trascriptionText.text = resultString
+                                
+                            }
+                            
+                        }
+                        
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func transcribeButtonPressed(_ sender: Any) {
+        
+        activitySpinner.isHidden = false
+        activitySpinner.startAnimating()
+        
+        requestSpeechAuth()
+        
     }
-
 
 }
 
